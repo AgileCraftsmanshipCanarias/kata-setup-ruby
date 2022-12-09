ef05bc9f070c92a862734030581d5482473a1ad5
Daniel Ramos <danielramosacosta1@gmail.com>
2022-12-09 16:35:45 +0000

    implement mock test

--- lib/kata.rb
+++ lib/kata.rb
@@ -3,6 +3,26 @@
 require_relative "kata/version"
 
 module Kata
-  class Error < StandardError; end
-  # Your code goes here...
+  class ExampleClass
+    def initialize(dependency)
+      @dependency = dependency
+    end
+
+
+    def get_value
+      @dependency.get_value
+    end
+  end
+
+  class Dependency
+    def get_value
+      raise NotImplementedError.new("You must implement the #{self.class} interface")
+    end
+  end
+
+  class DependencyImpl
+    def get_value
+      0
+    end
+  end
 end
--- spec/kata_spec.rb
+++ spec/kata_spec.rb
@@ -5,7 +5,22 @@ RSpec.describe Kata do
     expect(Kata::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(true).to eq(true)
+  it "works with manual mocking" do
+    dependency = Kata::DependencyImpl.new
+    example_class = Kata::ExampleClass.new(dependency)
+
+    value = example_class.get_value
+
+    expect(value).to eq(0)
+  end
+
+  it "works with rspec mocking" do
+    dependency = double("dependency")
+    allow(dependency).to receive(:get_value).and_return(0)
+    example_class = Kata::ExampleClass.new(dependency)
+
+    value = example_class.get_value
+
+    expect(value).to eq(0)
   end
 end
